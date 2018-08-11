6c8e67e9bf0a17ff6f464e8a6eac7f6b1f3ffcbe
unknown <yizhiqing@gmail.com>
2018-08-11 11:17:09 +0900

    Added some more features.

--- App.js
+++ App.js
@@ -7,21 +7,47 @@ export default class App extends Component {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Cavendish_Banana_DS.jpg/1200px-Cavendish_Banana_DS.jpg'
     };
     return (
-      <View style={styles.container}>
-        <Text>This is my first mobile application.</Text>
-        <Image source={pic} style={{width: 193, height: 110}}/>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+      <View style={{flex: 1}}>
+        <View style={{flex:2}}>
+          <View style={styles.container}>
+            <Text>This is my first mobile application.</Text>
+            <Image source={pic} style={{width: 193, height: 110}}/>
+            <Greeting name='Rexxar' />
+          </View>
+        </View>
+        <View style={{flex:1, width: 150, height: 150, backgroundColor: 'steelblue'}} >
+          <View style={{flex: 1, flexDirection: 'row'}}>
+            <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+            <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+            <View style={{width: 50, height: 50, backgroundColor: 'red'}} />
+          </View>
+        </View>
+        <View style={{flex:1, width: 150, height: 150, backgroundColor: 'skyblue'}} />
       </View>
     );
   }
 }
 
 class Greeting extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
   render() {
+    let display = this.state.isShowingText ? this.props.text : 'I show!';
     return (
-      <Text>Hello {this.props.name}!</Text>
+      <View>
+        <Text>Hello {this.props.name}!</Text>
+        <Text style={styles.red}>{display}</Text>
+      </View>
     );
   }
 }
@@ -32,5 +58,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  red: {
+    color: 'red',
   },
 });
